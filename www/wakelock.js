1f5f5ed46997afc5c28b669192eda8072334329f
jevian ma（马作伟） <jevian_ma@worldflying.cn>
2019-06-04 22:07:03 +0800

    检查bug1

--- www/wakelock.js
+++ www/wakelock.js
@@ -2,7 +2,12 @@ var exec = require('cordova/exec');
 
 exports = {
   coolMethod: function (arg0, success, error) {
-      exec(success, error, 'NativeCode', 'coolMethod', [arg0]);
+    // exec(success, error, 'NativeCode', 'coolMethod', [arg0]);
+    if (arg0.length > 0) {
+      success (arg0)
+    } else {
+      error ('arg0 is null')
+    }
   },
   acquire: function (timeout, flags, tag) {
     return new Promise (function (resolve, reject) {
@@ -32,6 +37,20 @@ exports = {
   WAKE_LOCK_LEVEL_MASK: 0x0000ffff,
   ACQUIRE_CAUSES_WAKEUP: 0x10000000,
   ON_AFTER_RELEASE: 0x20000000,
-  UNIMPORTANT_FOR_LOGGING: 0x40000000
+  UNIMPORTANT_FOR_LOGGING: 0x40000000,
+  RELEASE_FLAG_WAIT_FOR_NO_PROXIMITY: 1 << 0,
+  RELEASE_FLAG_TIMEOUT: 1 << 16,
+  BRIGHTNESS_ON: 255,
+  BRIGHTNESS_OFF: 0,
+  BRIGHTNESS_DEFAULT: -1,
+  USER_ACTIVITY_EVENT_OTHER: 0,
+  USER_ACTIVITY_EVENT_BUTTON: 1,
+  USER_ACTIVITY_EVENT_TOUCH: 2,
+  USER_ACTIVITY_EVENT_ACCESSIBILITY: 3,
+  USER_ACTIVITY_FLAG_NO_CHANGE_LIGHTS: 1 << 0,
+  USER_ACTIVITY_FLAG_INDIRECT: 1 << 1,
+  GO_TO_SLEEP_REASON_APPLICATION: 0,
+  GO_TO_SLEEP_REASON_DEVICE_ADMIN: 1,
+  GO_TO_SLEEP_REASON_TIMEOUT: 2  
 }
   
